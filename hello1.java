dbf78b2fa7fe9fa6637f56923266df29cc231a8c
Alexander <alexballinas@ymail.com>
2014-10-05 17:56:40 -0700

    09-09-14 Lab 1
    Write a program to print
    Hello
    My name is <your name goes here>

--- hello1.java
+++ hello1.java
@@ -5,6 +5,6 @@ public static void main(String[] args)
 System.out.println("Hello ");
 System.out.println("My Name Is Alexander");
 
-                        }
+                }
 
-                                }
+        }
