27291136dcd160494df473d536a2d2c047147e92
Alexander <alexballinas@ymail.com>
2014-10-05 18:02:46 -0700

    09-09-14 Lab 3
    Write a program to print “Hello World!” using variable, assignment statement and println output statement.

--- hello3.java
+++ hello3.java
@@ -4,5 +4,10 @@ public class Hello3 {
 public static void main(String[] args){
 
 
-System.out.printf("%s %s", "Hello", "World");
+System.out.printf("%s %s", "Hello", "World"); 
+  
+                                      }
+                    
+    
+                    }
 
